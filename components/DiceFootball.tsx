46db16263c6234c725f55a3ebc2e2701d59ea4fe
chezbro <ericchesbrough@gmail.com>
2024-10-26 13:27:51 -0700

    rolling

--- components/DiceFootball.tsx
+++ components/DiceFootball.tsx
@@ -243,7 +243,7 @@ const DiceFootball = () => {
                 '--velocity': dice.velocity,
                 transform: `translate(-50%, 0) translate(${dice.startX}px, ${dice.startY}px)`,
                 animation: `${dice.landingZone === 'off' ? 'rollOff' : 'rollAcross'} ${2 / dice.velocity}s forwards`
-              }}
+              } as React.CSSProperties}
             >
               {getDiceIcon(dice.value)}
             </div>
